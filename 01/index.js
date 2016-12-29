f3ad35c3f7d3d37372bc0ce409795bf527f481c2
纳兰天忆 <103520146@qq.com>
2016-12-29 16:56:07 +0800

    //

---
+++ 01/index.js
@@ -0,0 +1,24 @@
+/**
+ * Created by nalantianyi on 2016/12/29.
+ */
+import {createStore} from 'redux';
+import todoApp from './reducers';
+import {addTodo, toggleTodo, setVisibilityFilter, VisibilityFilters} from './actions';
+
+
+let store = createStore(todoApp);
+
+console.log(store.getState());
+let unsubscribe = store.subscribe(() => {
+    console.log(store.getState());
+});
+
+store.dispatch(addTodo('Learn about actions'))
+store.dispatch(addTodo('Learn about reducers'))
+store.dispatch(addTodo('Learn about store'))
+store.dispatch(toggleTodo(0))
+store.dispatch(toggleTodo(1))
+store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED));
+
+// 停止监听 state 更新
+unsubscribe();
