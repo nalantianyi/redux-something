2d60df9911a0df6ded259312604c874092cafa56
纳兰天忆 <103520146@qq.com>
2017-01-02 21:53:08 +0800

    //异步actions

--- 03/src/index.js
+++ 03/src/index.js
@@ -5,7 +5,7 @@ import 'babel-polyfill';
 import thunkMiddleware from 'redux-thunk';
 import createLogger from 'redux-logger';
 import {createStore, applyMiddleware} from 'redux';
-import {selectSubreddit, fetchPosts} from './actions';
+import {selectSubreddit, fetchPostsIfNeeded} from './actions';
 import  rootReducer from './reducers';
 
 const loggerMiddleware = createLogger();
@@ -14,4 +14,4 @@ const store = createStore(
     applyMiddleware(thunkMiddleware, loggerMiddleware)
 );
 store.dispatch(selectSubreddit('reactjs'));
-store.dispatch(fetchPosts('reactjs')).then(() => console.log(store.getState()));
+store.dispatch(fetchPostsIfNeeded('reactjs')).then(() => console.log(store.getState()));
